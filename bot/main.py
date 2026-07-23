1c01b7588744e8bdeac9afdba7bd323e279473f0
dotamind-ai <whoamivdk@yandex.ru>
2026-07-23 17:38:18 +0300

    Create main.py

---
+++ bot/main.py
@@ -0,0 +1,29 @@
+from telegram.ext import (
+    Application,
+    CommandHandler
+)
+
+from config import BOT_TOKEN
+from handlers import start_command
+
+
+def main():
+
+    app = Application.builder() \
+        .token(BOT_TOKEN) \
+        .build()
+
+    app.add_handler(
+        CommandHandler(
+            "start",
+            start_command
+        )
+    )
+
+    print("Dotamind bot started")
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
