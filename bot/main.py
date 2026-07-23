6fe0897acea7455a3ec11069de29b4eca300b96a
dotamind-ai <whoamivdk@yandex.ru>
2026-07-23 17:44:37 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,8 @@
 from telegram.ext import (
     Application,
-    CommandHandler
+    CommandHandler,
+    MessageHandler,
+    filters
 )
 
 from config import BOT_TOKEN
@@ -10,10 +12,16 @@ from analyzer_handler import analyze_match
 
 def main():
 
+    if not BOT_TOKEN:
+        print("Ошибка: BOT_TOKEN не найден")
+        return
+
     app = Application.builder() \
         .token(BOT_TOKEN) \
         .build()
 
+
+    # Команда /start
     app.add_handler(
         CommandHandler(
             "start",
@@ -21,8 +29,21 @@ def main():
         )
     )
 
-    print("Dotamind bot started")
 
+    # Любой текст после /start
+    # будет отправляться в анализатор
+    app.add_handler(
+        MessageHandler(
+            filters.TEXT & ~filters.COMMAND,
+            analyze_match
+        )
+    )
+
+
+    print("🤖 Dotamind AI Bot запущен")
+
+
+    # Запуск ожидания сообщений
     app.run_polling()
 
 
